1f09487f48f12a67648254906b6cb68395fe108a
sk0za <89702597+ulyssepmt@users.noreply.github.com>
2024-12-05 19:57:05 +0100

    Delete SNAKE directory

--- SNAKE/SNAKE/draw.h
+++
@@ -1,15 +0,0 @@
-#pragma once
-#include "game.h" 
-#include "resource.h"
-
-//§#define IDB_IMAGE1 129 
-
-void drawPixel(HDC hdc, int x, int y, int w, int h, HBRUSH color); 
-
-void putFood(struct FOOD* pFood, HDC hdc); 
-
-void printScore(HDC hdc, int score, char scoreBuf[15]); 
-
-void printText(HDC hdc, HBRUSH black, LPCWSTR msgToPlayer, int idText); 
-
-BOOL printMenu(); 

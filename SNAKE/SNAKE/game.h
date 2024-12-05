f9c33d78fb04f8ffa64431ef3f3df90bcb0b83bb
sk0za <89702597+ulyssepmt@users.noreply.github.com>
2024-12-05 21:09:04 +0100

    Add files via upload

---
+++ SNAKE/SNAKE/game.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "main.h"
+
+struct SNAKE {
+    int x;
+    int y;
+    int w;
+    int h;
+    struct SNAKE* next;
+};
+
+struct FOOD {
+    int x;
+    int y;
+    int w;
+    int h;
+    HBRUSH color;
+};
+
+DWORD WINAPI runGame(LPVOID lp); 
+
+BOOL checkCollision(int xObject1, int yObject1, int wObject1, int hObject1,
+    int xObject2, int yObject2, int wObject2, int hObject2); 
